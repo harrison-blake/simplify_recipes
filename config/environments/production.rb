8b79951ab941fe697ac512e53d0ee156ac670d46
Harrison Blake <harrison.r.blake@gmail.com>
2024-12-19 17:53:53 -0500

    add heroku to allowed hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,7 @@ Rails.application.configure do
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
+  config.hosts << "simplify-web-9147d8b72c6d.herokuapp.com"
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
