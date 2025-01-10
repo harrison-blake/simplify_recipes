468f6574b77174fb685ddeb9adb6a2f3f2c504a3
Harrison Blake <harrison.r.blake@gmail.com>
2025-01-10 11:18:48 -0500

    add error checking if api is down

--- app/services/recipe_service.rb
+++ app/services/recipe_service.rb
@@ -1,14 +1,19 @@
-class RecipeService
-  def self.get_recipe(url)
-    api_endpoint = Rails.application.config.api_endpoint
+class RecipeService def self.get_recipe(url) 
+api_endpoint = Rails.application.config.api_endpoint 
+uri = URI.parse(api_endpoint)
 
-    uri = URI.parse(api_endpoint)
-    response = Net::HTTP.post(
-      uri,
-      { url: url }.to_json,
-      "Content-Type" => "application/json"
-    )
-
-    JSON.parse(response.body)
+    begin
+      response = Net::HTTP.post(
+        uri,
+        { url: url }.to_json,
+        "Content-Type" => "application/json"
+      )
+    # if api is down log error and return error to controller
+    rescue Errno::ENETUNREACH => e
+      Rails.logger.error("Network unreachable: #{e.message}")
+      response = { error: "Internal service is currently unavailable.", status: :service_unavailable }
+    else
+      JSON.parse(response.body)
+    end
   end
 end
--- spec/services/recipe_service_spec.rb
+++ spec/services/recipe_service_spec.rb
@@ -11,15 +11,33 @@ RSpec.describe RecipeService do
   url = 'https://www.allrecipes.com/recipe/223042/chicken-parmesan/'
   data = []
 
-  VCR.use_cassette('chicken_parm') do
-    data = RecipeService.get_recipe(url)
-  end
-
-  describe '200 OK response (happy path)' do
+  describe '200 OK response' do
+    before do
+      VCR.use_cassette('http_success') do
+        data = RecipeService.get_recipe(url)
+      end
+    end
+      
     it 'should return data in the correct format' do
       expect(data).to be_a(Hash)
       expect(data).to have_key("converted")
       expect(data).to have_key("original")
+    end
+  end
+
+  # Net::HTTP does not trigger vcr to record a cassett if service is completely down
+  # stub a call instead
+  describe '503 Service Unavailable response' do
+    before do
+      allow(Net::HTTP).to receive(:post).and_raise(Errno::ENETUNREACH.new("Network is unreachable"))
+      data = RecipeService.get_recipe(url)  
+    end
+
+    it 'should return data in the correct format' do
+      expect(data).to be_a(Hash)
+      expect(data).to have_key(:error)
+      expect(data[:error]).to eq("Internal service is currently unavailable.")
+      expect(data[:status]).to eq(:service_unavailable)
     end
   end
 end
