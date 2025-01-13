cacc82ade8f5e0a25843a8cbb57302fab4575f8f
Harrison Blake <harrison.r.blake@gmail.com>
2025-01-13 13:42:37 -0500

    finally realized that heroku config vars aren't available during buildtime

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  # config.require_master_key = true
+  config.require_master_key = true
   secret_key_base = Rails.application.credentials.dig(:secret_key_base)
 
   # Code is not reloaded between requests.
