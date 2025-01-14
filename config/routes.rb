a2231fbf3ba46b145a6dad6d3d19d1eafa67b46e
Harrison Blake <harrison.r.blake@gmail.com>
2025-01-14 17:19:22 -0500

    started navbar and changed naming conventions on the home view

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: "home#index"
   resources :recipes, only: %i[ create show ]
 
+  get "login", to: "sessions#login"
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
